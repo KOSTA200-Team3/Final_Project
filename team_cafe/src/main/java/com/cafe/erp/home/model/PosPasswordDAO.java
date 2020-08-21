a2c8f99e9ad5cf0296af55aaab47222492daae3b
jih1729 <64896890+jih1729@users.noreply.github.com>
2020-08-21 09:12:16 +0900

    asd
    asdasd

--- team_cafe/src/main/java/com/cafe/erp/home/model/PosPasswordDAO.java
+++
@@ -1,8 +0,0 @@
-package com.cafe.erp.home.model;
-
-public interface PosPasswordDAO {
-
-	public String getPosPassword();							//db에서 password값 가져오기
-	public void updatePosPassword(String pwd);				//password 변경
-	
-}
